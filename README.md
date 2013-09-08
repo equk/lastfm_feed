b4afcbc87f20467f58d559a67dfb577f9c51849c
Bradley Walden <equk.co.uk@gmail.com>
2013-09-08 21:13:21 +0100

    :ship: initial commit

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
 lastfm_feed
 ===========
 
-simple lasftm feed using php + xml
+
+*****************************************************************
+                Lastfm feed - equk.co.uk
+*****************************************************************
+ lastfm feed - Copyright (C) 2013  Bradley Walden
+ This program comes with ABSOLUTELY NO WARRANTY.
+ This is free software, and you are welcome to redistribute it
+ under certain conditions. See GNU GPLv3.
+*****************************************************************
+
+*****************************************************************
+Version: 0.1.0
+
+Changelog:
+    0.1.0 - initial commit
+*****************************************************************
+
+  To use this script you need to include lastfm_feed.php.
+  You can then call the functions.
+
+  There is also a function for css styles which can be put into
+  the header of the website/page.
+
+  Please see example.php for a working example of this code.
+
+
+EXAMPLE:
+
+{% highlight php %}
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<?php include ('lastfm_feed.php') ?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <?php lastfmfeed_css();?>
+
+</head>
+<body>
+
+    <h1>last.fm</h1>
+
+    <?php lastfmfeed('equilibriumuk','10'); ?>
+
+</body>
+</html>
+
+{% endhighlight %}
---
+++ lastfm_feed.php
@@ -0,0 +1,89 @@
+<?php
+/*
+*****************************************************************
+                Lastfm feed - equk.co.uk
+*****************************************************************
+ lastfm feed - Copyright (C) 2013  Bradley Walden
+ This program comes with ABSOLUTELY NO WARRANTY.
+ This is free software, and you are welcome to redistribute it
+ under certain conditions. See GNU GPLv3.
+*****************************************************************
+
+
+                                      -oo-
+                                      +MMo
+      .:+o+:`   `:+o/-+o+  +oo`  ooo  +MMo .oo+.
+     oNNs+hMd- :mMmyydMMm  mMM`  MMM  +MMs/mMh:
+    :MMmyyhMMy mMM-  .MMm  mMM`  MMM  +MMmNMh`
+    -NMmo//++- hMMo..+MMm  dMM/.:MMM  +MMd+NMd-
+     :ymmmmdh` .smNmdyMMm  -dNNmhhMN  +MMo :mMN+
+       `...``    `..``MMm   ``.` `..  `..`  `...
+                     `NNd
+
+*****************************************************************
+Version: 0.1.0
+
+Changelog:
+    0.1.0 - initial commit
+*****************************************************************
+
+  To use this script you need to include this file using php.
+  You can then call the functions.
+
+  eg: lastfmfeed('equilibriumuk','10');
+
+  There is also a function for css styles which can be put into
+  the header of the website/page.
+
+  eg: lastfmfeed_css();
+
+  Please see example.php for a working example of this code.
+
+*/
+function lastfmfeed($lastfm_user,$lastfm_num) {
+    /* VARIABLES
+       =========
+
+       lastfm_user = lastfm username
+       lastfm_api = lastfm api key
+       lastfm_num = number of tracks to show
+
+       */
+
+    //$lastfm_user = 'equilibriumuk';
+    $lastfm_api = '';
+    //$lastfm_num = '10';
+
+    $lastfm_request = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=".$lastfm_user."&api_key=".$lastfm_api."&limit=".$lastfm_num."";
+    $xml = simplexml_load_file($lastfm_request);
+    $music = $xml->recenttracks->track;
+
+    /* END VARIABLES */
+
+    echo "<div id='lastfmfeed_wrapper'>";
+
+    for ($i = 0; $i < $lastfm_num; $i++) {
+
+
+        echo "<div class='lastfmfeed_recently_played'>";
+        if ($music[$i]->image <> ''){
+            echo "<img src='".$music[$i]->image."' alt='albumart'>";
+        } else {
+            echo "<img src='img/nocover.png' alt='albumart'>";
+        }
+        echo "<div class='lastfmfeed_song_info'>";
+        echo "<a href='".$music[$i]->url."' target='_blank'>" .$music[$i]->artist . " - ";
+        echo $music[$i]->name . "</a>";
+        echo "<div class='time'>".$music[$i]->date."</div>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+    echo "</div>";
+
+}
+
+function lastfmfeed_css(){
+  echo "<link href='css/lastfm_feed.css' rel='stylesheet'>";
+}
+?>
